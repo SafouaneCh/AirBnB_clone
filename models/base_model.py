86c6aedbc3e6c84c065b642d9295ec4123692ffc
SafoauneCh <safouanecharki3@gmail.com>
2024-07-15 11:54:42 +0100

    update

--- models/base_model.py
+++ models/base_model.py
@@ -6,20 +6,20 @@ class BaseModel:
         """Initilialize the BaseModel"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.update_at = self.created_at
+        self.updated_at = self.created_at
 
     def __str__(self):
         class_name = self.__class__.__name__
         return f"[{class_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         dict_representation = dict.__dict__.copy()
         dict_representation['__class__'] = self.__class__.__name__
 
         dict_representation['created_at'] = self.created_at.isoformat()
-        dict_representation['updated_at'] = self.update_at.isoformat()
+        dict_representation['updated_at'] = self.updated_at.isoformat()
 
         return dict_representation
